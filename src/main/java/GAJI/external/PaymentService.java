5d0379308a17ed4969f6f9bde43d1ff9344a412a
ilsongpark <darbysong@naver.com>
2020-09-08 10:22:42 +0900

    Config Map

--- src/main/java/GAJI/external/PaymentService.java
+++ src/main/java/GAJI/external/PaymentService.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="Payment", url="http://localhost:8084")
+//@FeignClient(name="Payment", url="http://localhost:8084")
+@FeignClient(name="Payment", url="${api.url.payment}")
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")
